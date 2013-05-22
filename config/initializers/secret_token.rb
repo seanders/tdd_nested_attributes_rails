bcab4736591855e7ef4774f269d2fe91f8c3b0e7
fmackojc <fab.mackojc@gmail.com>
2013-05-22 10:52:42 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddNest::Application.config.secret_token = '1901f68c3da4302a33be8bf6c691c7e873f5e13af6644924a3922d3e9b6a9f79e56f94c9bbe5d1fa149daee92a4471dad37d572df9c3c24faa6b1a5119e55e81'
